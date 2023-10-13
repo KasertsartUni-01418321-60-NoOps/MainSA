93b5aad9dbfbc35f4ec25179ba3b889b594ee42d
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-10-13 11:39:15 +0700

    Update code

--- src/java/Main.java
+++ src/java/Main.java
@@ -1,20 +1,22 @@
-public class Main {
+public class Main extends javafx.application.Application {
     private static boolean isFatal = false;
+
     public static void main(String[] args) throws Throwable {
         try {
             // TODO: Exception System
             try {
-                Main.temp();
+                tempDatabase();
+                launch(args);
             } catch (Error e) {
-                Main.isFatal=true;
+                Main.isFatal = true;
                 e.printStackTrace();
                 throw e;
             } catch (Exception e) {
-                Main.isFatal=true;
+                Main.isFatal = true;
                 e.printStackTrace();
                 throw e;
             } catch (Throwable e) {
-                Main.isFatal=true;
+                Main.isFatal = true;
                 e.printStackTrace();
                 throw e;
             }
@@ -26,33 +28,71 @@ public class Main {
                     // due to we wait until window is closed // System.exit(0);
                 }
             } catch (Throwable e) {
-                try { System.exit(255); }
-                finally { throw e; }
+                try {
+                    System.exit(255);
+                } finally {
+                    throw e;
+                }
             }
         }
     }
-    public static void temp() throws Throwable {
-        // Create a JFrame (window)
-        final javax.swing.JFrame mainWindow = new javax.swing.JFrame("Hello World GUI");
-        mainWindow.setSize(400, 150);
-        mainWindow.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
-        final javax.swing.JPanel mainWindow_MainPanel = new javax.swing.JPanel();
-        mainWindow_MainPanel.setLayout(new java.awt.FlowLayout());
-        final javax.swing.JLabel mainWindow_Label1 = new javax.swing.JLabel("Hello, World!");
-        final javax.swing.JButton mainWindow_Button1= new javax.swing.JButton("Test Me!");
-        mainWindow_Button1.addActionListener(new java.awt.event.ActionListener() {
-            @Override
-            public void actionPerformed(java.awt.event.ActionEvent e)  {
-                try {
-                java.awt.Desktop.getDesktop().browse( new java.net.URI("https://www.youtube.com/watch?v=oHg5SJYRHA0") );
-                } catch (Throwable e1) {}
-            }
-        });
-        mainWindow_MainPanel.add(mainWindow_Label1);
-        mainWindow_MainPanel.add(mainWindow_Button1);
-        mainWindow.add(mainWindow_MainPanel);
-        mainWindow.setVisible(true);
 
+    @Override
+    public void start(javafx.stage.Stage primaryStage) {
+        try {
+            // TODO: Exception System
+            try {
+                primaryStage.setTitle("<App Name>");
+
+                javafx.scene.control.Button Button1 = new javafx.scene.control.Button("Click me!");
+                Button1.setOnAction(e -> tempOpenWebPage("https://www.youtube.com/watch?v=oHg5SJYRHA0"));
+
+                javafx.scene.layout.StackPane MainScene_MainRoot = new javafx.scene.layout.StackPane();
+                MainScene_MainRoot.getChildren().add(Button1);
+
+                javafx.scene.Scene MainScene = new javafx.scene.Scene(MainScene_MainRoot, 400, 200);
+                primaryStage.setScene(MainScene);
+
+                primaryStage.show();
+            } catch (Error e) {
+                Main.isFatal = true;
+                e.printStackTrace();
+                throw e;
+            } catch (Exception e) {
+                Main.isFatal = true;
+                e.printStackTrace();
+                throw e;
+            } catch (Throwable e) {
+                Main.isFatal = true;
+                e.printStackTrace();
+                throw e;
+            }
+        } finally {
+            try {
+                if (Main.isFatal) {
+                    System.exit(255);
+                } else {
+                    // due to we wait until window is closed // System.exit(0);
+                }
+            } catch (Throwable e) {
+                try {
+                    System.exit(255);
+                } finally {
+                    throw e;
+                }
+            }
+        }
+    }
+
+    private static void tempOpenWebPage(String url) {
+        try {
+            java.awt.Desktop.getDesktop().browse(new java.net.URI(url));
+        } catch (java.io.IOException | java.net.URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void tempDatabase() throws Throwable {
         // Connect to the SQLite database or create it if it doesn't exist
         Class.forName("org.sqlite.JDBC");
         java.sql.Connection mainDbConn = java.sql.DriverManager.getConnection("jdbc:sqlite:./data/main.db");
