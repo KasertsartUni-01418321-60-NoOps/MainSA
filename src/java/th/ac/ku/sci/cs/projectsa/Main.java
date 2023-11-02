c505ab9283adb12b3c0e4f05e1c51604299873f3
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-02 09:11:52 +0700

    เพิ่ม UI to code

--- src/java/th/ac/ku/sci/cs/projectsa/Main.java
+++ src/java/th/ac/ku/sci/cs/projectsa/Main.java
@@ -96,13 +96,15 @@ public class Main extends javafx.application.Application {
             com.github.saacsos.FXRouter.bind(this, primaryStage,
                     "ระบบหลังบ้านบริการซื้อขายเครื่องซักผ้าอุตสาหกรรมมือสอง", 800, 600);
             // do not put leading slash for jarfile resource for this line of code
-            com.github.saacsos.FXRouter.when("homepage", "resources/homepage_pre.fxml");
             // TODO: [UI] adding page lamo
+            for (String pageName : new String[] {"homepage","login","add_item","buy_history","check_items","create_customer","customer_data","customer_list","money_accounting","quotation","sell_history","warehouse"}) {
+                com.github.saacsos.FXRouter.when(pageName, "resources/"+pageName+"_pre.fxml");
+            }
             // TODO: [UI] adding controller
             try
 
             {
-                com.github.saacsos.FXRouter.goTo("homepage");
+                com.github.saacsos.FXRouter.goTo("login");
             } catch (java.io.IOException e) {
                 throw e;
             }
