443024e7481a6b3790cbbb48bc2fdec8b2753150
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-03 21:54:45 +0700

    update

--- runInCmd.bat
+++ runInCmd.bat
@@ -2,8 +2,6 @@
 @title NP-chaonay Java Building System : Application Executing
 @color 0A
 set "JAVA_HOME=%HOME%\Downloads\JAVA\openlogic-openjdk-8u382-b05-windows-64\"
-@echo [NPc] (Cancelled) Set to JRE 8 by java.com because it have more complete JavaFX installation lamo
-@REM set "JAVA_HOME=C:\Program Files\Java\jre-1.8\"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 cd build/final
 java -jar App.jar
