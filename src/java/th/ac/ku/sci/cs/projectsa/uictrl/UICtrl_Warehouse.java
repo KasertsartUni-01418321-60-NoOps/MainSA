e1819e39aa595d1e0f9e4cb7634e1c8ed2b107c1
Netichai <netichai2003@gmail.com>
2023-11-05 11:49:06 +0700

    test scrollPane on warehouse

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
@@ -10,8 +10,23 @@ import javafx.fxml.*;
 import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.paint.Color;
+// ทดสอบ scrollPane
+ import javafx.scene.shape.Rectangle;
 
 public class UICtrl_Warehouse {
+    @FXML 
+    private TextField brand, model, status;
+    @FXML
+    private ScrollPane scrollPaneWarehouse;
+
+    @FXML
+    private void initialize(){
+        // ทดสอบ scrollPane
+        Rectangle rect = new Rectangle(200, 200, Color.RED);
+        scrollPaneWarehouse.setContent(rect);
+    }
+
     @FXML private void onBack_Button() throws java.sql.SQLException, java.security.NoSuchAlgorithmException, Throwable {
         try {
             Main.switchToSpecificPagename("homepage");
@@ -19,5 +34,25 @@ public class UICtrl_Warehouse {
             MyExceptionHandling.handleFatalException(e);
             throw e;
         }
+    }
+    @FXML private void onPressed_Button_addProduct() throws java.io.IOException {
+        try {
+            Main.switchToSpecificPagename("add_item");
+        } catch (Throwable e) {
+			MyExceptionHandling.handleFatalException(e);
+            throw e;
+        }
+    }
+
+    @FXML private void onPressed_Button_findBrand() throws java.io.IOException {
+        System.out.println("finding brand");
+    }
+
+    @FXML private void onPressed_Button_findModel() throws java.io.IOException {
+        System.out.println("finding model");
+    }
+
+     @FXML private void onPressed_Button_findStatus() throws java.io.IOException {
+        System.out.println("finding status");
     }
 }
