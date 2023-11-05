7e42da9bc0b454748de5e9cb5c87dad6cd38a4fa
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-05 13:58:15 +0700

    UI stuff

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_CustomerList.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_CustomerList.java
@@ -1,18 +1,11 @@
 package th.ac.ku.sci.cs.projectsa.uictrl;
-import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
-import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataSpec.STATUS_User;
-
-import com.github.saacsos.FXRouter;
-
 import javafx.fxml.*;
-import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.input.MouseEvent;
 
 public class UICtrl_CustomerList {
-    @FXML private void backHomepage(MouseEvent event) throws java.sql.SQLException, java.security.NoSuchAlgorithmException, Throwable {
+    // REMARK: เราจะcopy function นี้ไปใช้ได้เลย เวลาต้องการ backToHomePage
+    @FXML private void backHomepage() throws java.io.IOException {
         try {
             Main.switchToSpecificPagename("homepage");
         } catch (Throwable e) {
