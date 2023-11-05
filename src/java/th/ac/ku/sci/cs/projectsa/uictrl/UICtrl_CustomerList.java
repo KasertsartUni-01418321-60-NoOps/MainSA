d7a4239c76b4662df21cb8205e6dffc030efe63c
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-06 01:41:32 +0700

    แก้ UI

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_CustomerList.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_CustomerList.java
@@ -9,38 +9,21 @@ import javafx.scene.input.MouseEvent;
 
 public class UICtrl_CustomerList {
     @FXML private ListView<ListViewRowDataWrapper> custListView;
+    @FXML private TextField textField_SearchBox;
     
     @FXML
     private void initialize() throws java.sql.SQLException{
         try {
-			DatabaseMnm.Table tmpc_SQLTable = null;
-			try {
-				tmpc_SQLTable = (DatabaseMnm.Table) (DatabaseMnm.runSQLcmd(
-						null,
-						"SELECT Customer_Full_Name FROM Customer",
-						false,
-						true,
-						null,
-						null)[1]);
-			} catch (java.sql.SQLException e) {
-				MyExceptionHandling.handleFatalException_simplev1(e, true, "MainApp|DatabaseMnm", null, null,
-						"<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดเช็คความถูกต้องของไฟล์โปรแกรมและข้อมูลและเช็คว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
-				throw e;
-			}
-            int tmpl_0=tmpc_SQLTable.cols[0].vals.size();
-            ListViewRowDataWrapper[] tmpc_SQLTable__listViewRowDataWrapper = new ListViewRowDataWrapper[tmpl_0];
-            for (int tmpc_int =0; tmpc_int<tmpl_0; tmpc_int++) {
-                String tmpt_str=(String)(tmpc_SQLTable.cols[0].vals.get(tmpc_int));
-                tmpc_SQLTable__listViewRowDataWrapper[tmpc_int]=new ListViewRowDataWrapper(tmpt_str, tmpt_str);
-            }
-            custListView.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
+			helper_listViewUpdate("");
             custListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
                     try {
                         if (event.getClickCount() >= 2) {
                             ListViewRowDataWrapper tmpt_lvrdw = custListView.getSelectionModel().getSelectedItem();
+                            if (tmpt_lvrdw!=null) {
                             Main.switchToSpecificPagename("customer_data",tmpt_lvrdw);
+                            }
                         }
                     } catch (Throwable e) {
                         MyExceptionHandling.handleFatalException(e);
@@ -69,6 +52,39 @@ public class UICtrl_CustomerList {
             MyExceptionHandling.handleFatalException(e);
             throw e;
         }
+    }
+    @FXML private void searchCustomer_Button() throws java.sql.SQLException{
+        try {
+            helper_listViewUpdate(textField_SearchBox.getText());
+        } catch (Throwable e) {
+            MyExceptionHandling.handleFatalException(e);
+            throw e;
+        }
+    }
+    private void helper_listViewUpdate(String searchString) throws java.sql.SQLException{
+        custListView.getItems().clear();
+        DatabaseMnm.Table tmpc_SQLTable = null;
+        try {
+            tmpc_SQLTable = (DatabaseMnm.Table) (DatabaseMnm.runSQLcmd(
+                    null,
+                    "SELECT Customer_Full_Name FROM Customer WHERE Customer_Full_Name LIKE ?",
+                    false,
+                    true,
+                    null,
+                    new Object[] {"%"+searchString+"%"}
+            )[1]);
+        } catch (java.sql.SQLException e) {
+            MyExceptionHandling.handleFatalException_simplev1(e, true, "MainApp|DatabaseMnm", null, null,
+                    "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดเช็คความถูกต้องของไฟล์โปรแกรมและข้อมูลและเช็คว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
+            throw e;
+        }
+        int tmpl_0=tmpc_SQLTable.cols[0].vals.size();
+        ListViewRowDataWrapper[] tmpc_SQLTable__listViewRowDataWrapper = new ListViewRowDataWrapper[tmpl_0];
+        for (int tmpc_int =0; tmpc_int<tmpl_0; tmpc_int++) {
+            String tmpt_str=(String)(tmpc_SQLTable.cols[0].vals.get(tmpc_int));
+            tmpc_SQLTable__listViewRowDataWrapper[tmpc_int]=new ListViewRowDataWrapper(tmpt_str, tmpt_str);
+        }
+        custListView.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
     }
 }
 
