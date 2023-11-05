a925da6b6d181677b825b45c9cf3e1c146e6bdde
Netichai <netichai2003@gmail.com>
2023-11-05 22:55:08 +0700

    .bat problem fuk!

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @REM this file is for sample only, do not overwrite if it is specific to your machine's hw/sw config; just make your own file and add .gitignore
 @title NP-chaonay Java Building System : Application Building
 @color 0A
-set "JAVA_HOME=%HOME%\Downloads\JAVA\openlogic-openjdk-8u382-b05-windows-64\"
+set "JAVA_HOME=C:\Program Files\Java\jdk-1.8\openlogic-openjdk-8u392-b08-windows-64"
 set "PATH=%JAVA_HOME%\bin;%PATH%"
 rmdir /S /Q build
 mkdir build
