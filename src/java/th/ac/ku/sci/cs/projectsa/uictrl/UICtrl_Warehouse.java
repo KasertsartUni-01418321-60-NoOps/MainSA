dc4f38a1a945078bf964f31af7df1bfa54052117
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-05 13:58:40 +0700

    ui edit

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
@@ -1,15 +1,8 @@
 package th.ac.ku.sci.cs.projectsa.uictrl;
 
-import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
-import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataSpec.STATUS_User;
-
-import com.github.saacsos.FXRouter;
-
 import javafx.fxml.*;
-import javafx.scene.chart.PieChart.Data;
 import javafx.scene.control.*;
-import javafx.scene.control.Alert.AlertType;
 import javafx.scene.paint.Color;
 // ทดสอบ scrollPane
  import javafx.scene.shape.Rectangle;
