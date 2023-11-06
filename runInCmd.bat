c74cf69e97b47fbced8cd34a90dfcc7e2279a21f
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-06 09:52:44 +0700

    Adding InteliJ stuff (setup except Jarfile) + deelete .bat

--- runInCmd.bat
+++
@@ -1,9 +0,0 @@
-@REM this file is for sample only, do not overwrite if it is specific to your machine's hw/sw config; just make your own file and add .gitignore
-@title NP-chaonay Java Building System : Application Executing
-@color 0A
-set "JAVA_HOME=D:\jdk8\openlogic-openjdk-8u392-b08-windows-64"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
-cd build/final
-java -jar App.jar -MiscFunFlag+muteMIDI
-@echo [SCRIPT] (Process ended)
-@pause
