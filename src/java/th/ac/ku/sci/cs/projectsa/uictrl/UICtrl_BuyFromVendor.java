0e5537d70198a667a93f33be750e941a0e739ce6
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-06 20:00:40 +0700

    partial edit

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_BuyFromVendor.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_BuyFromVendor.java
@@ -2,12 +2,33 @@ package th.ac.ku.sci.cs.projectsa.uictrl;
 
 import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
-
 import javafx.fxml.*;
 import javafx.scene.control.*;
+import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory;
 
 public class UICtrl_BuyFromVendor {
+    @FXML private TextField textField_custName;
+    @FXML private TextField textField_PdId;
+    @FXML private TextArea textArea_MeetLoc;
+    @FXML private TextArea textArea_PdLooks;
+    @FXML private Spinner<Double> spinnerDouble_PaidPrice;
+    @FXML private DatePicker datePicker_MeetDate;
+
+    @FXML private void initialize() {
+        try{
+            spinnerDouble_PaidPrice.setValueFactory(new DoubleSpinnerValueFactory(0.01, 99999999.99, 25000.0, 100.0));
+        } catch (Throwable e) {
+            MyExceptionHandling.handleFatalException(e);
+            throw e;
+        }
+    }
+
     @FXML private void onBack_Button() throws java.sql.SQLException, java.security.NoSuchAlgorithmException, Throwable {
+        // มันไม่ update ให้ update เองแม่งๆ
+        spinnerDouble_PaidPrice.increment(0);
+        System.out.println(
+            datePicker_MeetDate.getValue().toEpochDay()*86400+43200-1
+        );
         try {
             Main.switchToSpecificPagename("homepage");
         } catch (Throwable e) {
@@ -15,4 +36,6 @@ public class UICtrl_BuyFromVendor {
             throw e;
         }
     }
+    @FXML private void onpressed_Button_Save() {}
 }
+
