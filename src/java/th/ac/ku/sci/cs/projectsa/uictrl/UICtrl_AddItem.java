4bbc6107f1e942d9413fbddfeda17af772cc88a6
Netichai <netichai2003@gmail.com>
2023-11-06 21:32:54 +0700

    implement add item page, database remaining

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
@@ -4,13 +4,52 @@ import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
 
 import javafx.fxml.*;
-import th.ac.ku.sci.cs.projectsa.MyExceptionHandling;
 import javafx.scene.control.*;
+import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory;
+
+import java.util.Random;
 
 public class UICtrl_AddItem {
+
+    @FXML private TextField productID;
+    @FXML private TextArea repairDetail;
+    @FXML private Spinner<Double> creatPrice;
+    private String price;
+
+    @FXML
+    private void initialize() {
+       // generateID()
+        String randomeID = Misc.generateRandomID();
+        productID.setEditable(false);
+        productID.setText(randomeID);
+
+        try{
+            creatPrice.setEditable(true);
+            creatPrice.setValueFactory(new DoubleSpinnerValueFactory(0.01, 99999999.99, 25000.0, 100.0));
+            creatPrice.valueProperty().addListener((observable, oldValue, newValue) -> {
+                price = newValue.toString();
+            });
+        } catch (Throwable e) {
+            MyExceptionHandling.handleFatalException(e);
+            throw e;
+        }
+    }
+
+
     @FXML private void onBack_Button() throws java.sql.SQLException, java.security.NoSuchAlgorithmException, Throwable {
         try {
             Main.switchToSpecificPagename("homepage");
+        } catch (Throwable e) {
+            MyExceptionHandling.handleFatalException(e);
+            throw e;
+        }
+    }
+
+    @FXML private void onpressed_Button_Save() throws java.sql.SQLException, java.security.NoSuchAlgorithmException, Throwable {
+        try {
+            // TODO: save into database
+            System.out.println(repairDetail.getText());
+            System.out.println(price);
         } catch (Throwable e) {
             MyExceptionHandling.handleFatalException(e);
             throw e;
