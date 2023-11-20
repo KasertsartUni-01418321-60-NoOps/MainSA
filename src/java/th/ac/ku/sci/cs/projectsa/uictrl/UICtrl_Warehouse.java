afeda4e33690eda76e895926774d0c36b54efcdb
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-20 13:15:37 +0700

    ทำ warehouse เสร็จแล้ว เหลือทำระบบ add_item  ก่อนจะไปส่วนที่เหลือ (accounting/พวก ประวัติการขาย)

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Warehouse.java
@@ -50,7 +50,7 @@ public class UICtrl_Warehouse {
                 checkBoxPdStatus3.setVisible(!tmpt_bool);
                 checkBoxPdStatus4.setVisible(!tmpt_bool);
             });
-			helper_listViewUpdate(0,null);
+			helper_listViewUpdate(0,new boolean[] {true,true,true,true});
             pdListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
                 public void handle(MouseEvent event) {
@@ -136,13 +136,23 @@ public class UICtrl_Warehouse {
             whereQuery="WHERE SR.Selling_Request_Model LIKE ?";
             SQLParams=new Object[] {"%"+fStr+"%"};
         } else {
-            whereQuery="WHERE Customer_Full_Name LIKE ?";
+            whereQuery="WHERE " +
+                "(Product_Status = 0 AND ?) OR " +
+                "(Product_Status = 1 AND ?) OR " +
+                "(Product_Status = 2 AND ?) OR " +
+                "(Product_Status = 3 AND ?)";
+            SQLParams=new Object[] {
+                fBoolArr[0]?1:0,
+                fBoolArr[1]?1:0,
+                fBoolArr[2]?1:0,
+                fBoolArr[3]?1:0
+            };
         }
         DatabaseMnm.Table tmpc_SQLTable = null;
         try {
             tmpc_SQLTable = (DatabaseMnm.Table) (DatabaseMnm.runSQLcmd(
                     null,
-                    "SELECT PD.Product_ID, PD.Product_Arrive_Time, PD.Product_Price, PD.Product_Status, PD.Selling_Request_ID, SR.Selling_Request_Brand,SR.Selling_Request_Model FROM Product AS PD LEFT JOIN Selling_Request AS SR ON PD.Selling_Request_ID = SR.Selling_Request_ID "+whereQuery+" ORDER BY CASE WHEN PD.Product_Status = 2 THEN 0 WHEN PD.Product_Status = 0 THEN 1 ELSE 2 END, ROWID DESC;",
+                    "SELECT PD.Product_ID, PD.Product_Arrive_Time, PD.Product_Status, SR.Selling_Request_Brand, SR.Selling_Request_Model, PD.Selling_Request_ID FROM Product AS PD LEFT JOIN Selling_Request AS SR ON PD.Selling_Request_ID = SR.Selling_Request_ID "+whereQuery+" ORDER BY CASE WHEN PD.Product_Status = 2 THEN 0 WHEN PD.Product_Status = 0 THEN 1 ELSE 2 END, PD.rowid DESC;",
                     false,
                     true,
                     null,
@@ -153,56 +163,45 @@ public class UICtrl_Warehouse {
                     "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดตรวจสอบความถูกต้องของไฟล์โปรแกรมและข้อมูลและตรวจสอบว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
             throw e;
         }
-        // [TODO START ZONE]
         int tmpl_0=tmpc_SQLTable.cols[0].vals.size();
         java.util.List<ListViewRowDataWrapper<String>> tmpc_SQLTable__listViewRowDataWrapper = new java.util.ArrayList<ListViewRowDataWrapper<String>>(tmpl_0);
         for (int tmpc_int =0; tmpc_int<tmpl_0; tmpc_int++) {
-            String tmpk_Selling_Request_ID=(String)(tmpc_SQLTable.cols[0].vals.get(tmpc_int)); 
-            String tmpk_Customer_Full_Name=(String)(tmpc_SQLTable.cols[1].vals.get(tmpc_int));
-            String tmpk_Selling_Request_Brand=(String)(tmpc_SQLTable.cols[2].vals.get(tmpc_int));
-            String tmpk_Selling_Request_Model=(String)(tmpc_SQLTable.cols[3].vals.get(tmpc_int));
-            DatabaseMnm.DataSpec.STATUS_Selling_Request tmpk_Selling_Request_Status=DatabaseMnm.DataSpec.STATUS_Selling_Request.values()[
+            // P1
+            String tmpk_PDID=(String)(tmpc_SQLTable.cols[0].vals.get(tmpc_int)); 
+            // P2
+            Long tmpu_epochTimeData = DatabaseMnm.convertIntegerAlikeSQLColToLong(
+                tmpc_SQLTable.cols[1].vals.get(tmpc_int),tmpc_SQLTable.cols[1].javaType
+            );
+            String tmpk_PDArriveDate= (java.time.Instant.ofEpochSecond(tmpu_epochTimeData)).atZone(java.time.ZoneOffset.UTC).toLocalDate().toString();
+            // P3
+            DatabaseMnm.DataSpec.STATUS_Product tmpk_PDStatus_javaType=DatabaseMnm.DataSpec.STATUS_Product.values()[
                 DatabaseMnm.convertIntegerAlikeSQLColToLong(
-                    tmpc_SQLTable.cols[4].vals.get(tmpc_int),
-                    tmpc_SQLTable.cols[4].javaType
+                    tmpc_SQLTable.cols[2].vals.get(tmpc_int),
+                    tmpc_SQLTable.cols[2].javaType
                 ).intValue()
             ];
-            DatabaseMnm.DataSpec.STATUS_Product tmpk_Product_Status=null;
-            Long tmpt_long=DatabaseMnm.convertIntegerAlikeSQLColToLong(
-                tmpc_SQLTable.cols[5].vals.get(tmpc_int),
-                tmpc_SQLTable.cols[5].javaType
-            );
-            if (tmpt_long!=null) {
-            tmpk_Product_Status=DatabaseMnm.DataSpec.STATUS_Product.values()[
-                tmpt_long.intValue()
-            ];
-            }
-            String tmpt_str_1=null;
-            if (tmpk_Selling_Request_Status==STATUS_Selling_Request.WaitForCheck) {tmpt_str_1=Misc.ThaiStr_DataSpec_Status_SR[0];}
-            else if (tmpk_Selling_Request_Status==STATUS_Selling_Request.Acceapted) {
-                if (tmpk_Product_Status==null) {tmpt_str_1="รอนำสินค้าเข้าคลัง";}
-                else if (tmpk_Product_Status==STATUS_Product.NotYetSale) {tmpt_str_1="นำสินค้าเข้าคลังแล้ว/"+Misc.ThaiStr_DataSpec_Status_pd[0];}
-                else if (tmpk_Product_Status==STATUS_Product.ForSale) {tmpt_str_1=Misc.ThaiStr_DataSpec_Status_pd[1];}
-                else if (tmpk_Product_Status==STATUS_Product.SaledAndWaitForSend) {tmpt_str_1=Misc.ThaiStr_DataSpec_Status_pd[2];}
-                else {tmpt_str_1=Misc.ThaiStr_DataSpec_Status_pd[3];}
-            }
-            else {tmpt_str_1=Misc.ThaiStr_DataSpec_Status_SR[1];}
-            Long tmpu_epochTimeData = DatabaseMnm.convertIntegerAlikeSQLColToLong(
-                tmpc_SQLTable.cols[6].vals.get(tmpc_int),tmpc_SQLTable.cols[6].javaType
-            );
-            String tmpk_dateStr= (java.time.Instant.ofEpochSecond(tmpu_epochTimeData)).atZone(java.time.ZoneOffset.UTC).toLocalDate().toString();
-            String tmpk_repr="(นัดตรวจสอบสภาพเมื่อ "+tmpk_dateStr+") ["+tmpk_Selling_Request_ID+": "+tmpt_str_1+"] โดยคุณ \""+tmpk_Customer_Full_Name+"\" (ยี่ห้อ/รุ่น: "+tmpk_Selling_Request_Brand+"/"+tmpk_Selling_Request_Model+")";
+            String tmpk_PDStatus=null;
+            if (tmpk_PDStatus_javaType==STATUS_Product.NotYetSale) {tmpk_PDStatus=Misc.ThaiStr_DataSpec_Status_pd[0];}
+            else if (tmpk_PDStatus_javaType==STATUS_Product.ForSale) {tmpk_PDStatus=Misc.ThaiStr_DataSpec_Status_pd[1];}
+            else if (tmpk_PDStatus_javaType==STATUS_Product.SaledAndWaitForSend) {tmpk_PDStatus=Misc.ThaiStr_DataSpec_Status_pd[2];}
+            else {tmpk_PDStatus=Misc.ThaiStr_DataSpec_Status_pd[3];}
+            // P4
+            String tmpk_Brand=(String)(tmpc_SQLTable.cols[3].vals.get(tmpc_int));
+            // P5
+            String tmpk_Model=(String)(tmpc_SQLTable.cols[4].vals.get(tmpc_int));
+            // P6
+            String tmpk_SRID=(String)(tmpc_SQLTable.cols[5].vals.get(tmpc_int));
+            String tmpk_repr="(มาถึงเมื่อ "+tmpk_PDArriveDate+") ["+tmpk_PDID+": "+tmpk_PDStatus+"] จากคำสั่งซื้อ "+tmpk_SRID+" (ยี่ห้อ/รุ่น: "+tmpk_Brand+"/"+tmpk_Model+")";
             tmpc_SQLTable__listViewRowDataWrapper.add(
-                new ListViewRowDataWrapper<String>(tmpk_Selling_Request_ID, tmpk_repr)
+                new ListViewRowDataWrapper<String>(tmpk_PDID, tmpk_repr)
             );
         }
-        // [TODO END ZONE]
         pdListView.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
     }
     private void helper_changePageForViewDataOfRow() throws java.io.IOException {
         ListViewRowDataWrapper<String> tmpt_lvrdw = pdListView.getSelectionModel().getSelectedItem();
         if (tmpt_lvrdw!=null) {
-        Main.switchToSpecificPagename("product_detail",new Object[] {this.getClass(),tmpt_lvrdw});
+        Main.switchToSpecificPagename("product_detail",new Object[] {this.getClass(),tmpt_lvrdw.ref});
         }
     }
 }
