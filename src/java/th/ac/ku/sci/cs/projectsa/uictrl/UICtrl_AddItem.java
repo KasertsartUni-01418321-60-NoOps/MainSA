1208586115a7a8e075f0515c0d6065812024061b
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-20 22:17:47 +0700

    Intermediate edit of 1 ui class

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
@@ -13,11 +13,9 @@ public class UICtrl_AddItem {
     @FXML private TextArea rpDetail;
     @FXML private Spinner<Double> sellPrice;
 
-    // TODO:
     @FXML private void initialize() throws java.sql.SQLException{
         try{
-            comboBox_custName_Helper1();
-            datePicker_MeetDate.setValue(java.time.LocalDate.now().plusDays(1));
+            comboBox_srID_Helper1();
             
         } catch (Throwable e) {
             MyExceptionHandling.handleFatalException(e);
@@ -35,120 +33,118 @@ public class UICtrl_AddItem {
     }
     // TODO:
     @FXML private void onpressed_Button_Save() throws java.io.IOException,java.sql.SQLException {
-        try {
-            // Note that this always got from DB, so no validation
-            String formval_custName=null;
-            try {formval_custName = comboBox_custName.getSelectionModel().getSelectedItem().ref;}
-            catch (NullPointerException e) {
-                helper2();
-                return;
-            }
-            String formval_brand = textField_brand.getText();
-            String formval_model = textField_model.getText();
-            Long formval_meetDate= datePicker_MeetDate.getValue().toEpochDay()*86400+43200-1;
-            String formval_custLoc = textArea_MeetLoc.getText();
-            String formval_productLooks = textArea_PdLooks.getText();
-            // [VALIDZONE]
-            DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
-            // DISABLED: due to above reason
-            // DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
-            // tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Customer_Full_Name(formval_custName);
-            // if (tmpReason ==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.VALUE_NOT_EXISTED_AT_REFERENCED_COL) {
-            //     helper2();
-            //     return;
-            // } else if (tmpReason != null) {
-            //     helper1();
-            //     return;
-            // }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Brand(formval_brand);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Model(formval_model);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Date(formval_meetDate);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            if (!DatabaseMnm.DataValidation.ForMoreBussinessConstraint.checkDateAsEpochTimeIsNotPast(formval_meetDate)) {
-                helper3();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Location(formval_custLoc);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Product_Looks(formval_productLooks);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            // [END VALID ZONE]
-            String tmpt_str = null;
-            while (true) {
-                tmpt_str= Misc.generateRandomID();
-                tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_ID(tmpt_str);
-                if (tmpReason==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.REPEATED_VAL_OF_COL_PK) {
-                    continue;
-                } else if (tmpReason!=null) {
-                    throw new MyExceptionHandling.UserRuntimeException("Code should not be unreachable here.");
-                } else { break;}
-            }
-            try{
-                DatabaseMnm.runSQLcmd(
-                    null,
-                    "INSERT INTO Selling_Request VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?);",
-                    true,
-                    false,
-                    null,
-                    new Object[] {
-                        tmpt_str,
-                        formval_custName,
-                        formval_brand,
-                        formval_model,
-                        formval_productLooks,
-                        formval_meetDate,
-                        formval_custLoc,
-                        Double.class,
-                        (long)0,
-                        String.class
-                    }
-                );
-                DatabaseMnm.mainDbConn.commit();
-            } catch (java.sql.SQLException e) {
-                MyExceptionHandling.handleFatalException_simplev1(e, true, "MainApp|DatabaseMnm", null, null,
-                "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดตรวจสอบความถูกต้องของไฟล์โปรแกรมและข้อมูลและตรวจสอบว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
-                throw e;
-            };
-            Main.switchToSpecificPagename("buy_history");
-        } catch (Throwable e) {
-            MyExceptionHandling.handleFatalException(e);
-            throw e;
-        }
+        // try {
+        //     // Note that this always got from DB, so no validation
+        //     String formval_custName=null;
+        //     try {formval_custName = comboBox_custName.getSelectionModel().getSelectedItem().ref;}
+        //     catch (NullPointerException e) {
+        //         helper2();
+        //         return;
+        //     }
+        //     String formval_brand = textField_brand.getText();
+        //     String formval_model = textField_model.getText();
+        //     Long formval_meetDate= datePicker_MeetDate.getValue().toEpochDay()*86400+43200-1;
+        //     String formval_custLoc = textArea_MeetLoc.getText();
+        //     String formval_productLooks = textArea_PdLooks.getText();
+        //     // [VALIDZONE]
+        //     DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
+        //     // DISABLED: due to above reason
+        //     // DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
+        //     // tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Customer_Full_Name(formval_custName);
+        //     // if (tmpReason ==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.VALUE_NOT_EXISTED_AT_REFERENCED_COL) {
+        //     //     helper2();
+        //     //     return;
+        //     // } else if (tmpReason != null) {
+        //     //     helper1();
+        //     //     return;
+        //     // }
+        //     tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Brand(formval_brand);
+        //     if (tmpReason != null) {
+        //         helper1();
+        //         return;
+        //     }
+        //     tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Model(formval_model);
+        //     if (tmpReason != null) {
+        //         helper1();
+        //         return;
+        //     }
+        //     tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Date(formval_meetDate);
+        //     if (tmpReason != null) {
+        //         helper1();
+        //         return;
+        //     }
+        //     if (!DatabaseMnm.DataValidation.ForMoreBussinessConstraint.checkDateAsEpochTimeIsNotPast(formval_meetDate)) {
+        //         helper3();
+        //         return;
+        //     }
+        //     tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Location(formval_custLoc);
+        //     if (tmpReason != null) {
+        //         helper1();
+        //         return;
+        //     }
+        //     tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Product_Looks(formval_productLooks);
+        //     if (tmpReason != null) {
+        //         helper1();
+        //         return;
+        //     }
+        //     // [END VALID ZONE]
+        //     String tmpt_str = null;
+        //     while (true) {
+        //         tmpt_str= Misc.generateRandomID();
+        //         tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_ID(tmpt_str);
+        //         if (tmpReason==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.REPEATED_VAL_OF_COL_PK) {
+        //             continue;
+        //         } else if (tmpReason!=null) {
+        //             throw new MyExceptionHandling.UserRuntimeException("Code should not be unreachable here.");
+        //         } else { break;}
+        //     }
+        //     try{
+        //         DatabaseMnm.runSQLcmd(
+        //             null,
+        //             "INSERT INTO Selling_Request VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?);",
+        //             true,
+        //             false,
+        //             null,
+        //             new Object[] {
+        //                 tmpt_str,
+        //                 formval_custName,
+        //                 formval_brand,
+        //                 formval_model,
+        //                 formval_productLooks,
+        //                 formval_meetDate,
+        //                 formval_custLoc,
+        //                 Double.class,
+        //                 (long)0,
+        //                 String.class
+        //             }
+        //         );
+        //         DatabaseMnm.mainDbConn.commit();
+        //     } catch (java.sql.SQLException e) {
+        //         MyExceptionHandling.handleFatalException_simplev1(e, true, "MainApp|DatabaseMnm", null, null,
+        //         "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดตรวจสอบความถูกต้องของไฟล์โปรแกรมและข้อมูลและตรวจสอบว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
+        //         throw e;
+        //     };
+        //     Main.switchToSpecificPagename("buy_history");
+        // } catch (Throwable e) {
+        //     MyExceptionHandling.handleFatalException(e);
+        //     throw e;
+        // }
     }
-    // TODO:
     private void helper1() {
         Main.showAlertBox(Main.getPrimaryStage(), AlertType.ERROR, "การเพิ่มข้อมูลผิดพลาด",
-						"ไม่สามารถเพ่ิมข้อมูลสัญญาชื้อได้", "เนื่องจากกรอกข้อมูลผิดรูปแบบ", false);
+						"ไม่สามารถเพ่ิมข้อมูลสินค้าเข้าคลังได้", "เนื่องจากกรอกข้อมูลผิดรูปแบบ", false);
     }
     // REMARK: เผื่อกรณี NULL ของ ComboBox
     private void helper2() {
         Main.showAlertBox(Main.getPrimaryStage(), AlertType.ERROR, "การเพิ่มข้อมูลผิดพลาด",
 						"ไม่สามารถเพ่ิมข้อมูลสินค้าเข้าคลังได้", "เนื่องจาก ID สัญญาซื้อ ยังไม่ได้ถูกเลือก", false);
     }
-    // TODO:
-    private void comboBox_custName_Helper1() throws java.sql.SQLException {
+    private void comboBox_srID_Helper1() throws java.sql.SQLException {
         DatabaseMnm.Table tmpc_SQLTable = null;
         try {
             tmpc_SQLTable = (DatabaseMnm.Table) (DatabaseMnm.runSQLcmd(
                     null,
-                    "SELECT Customer_Full_Name FROM Customer ORDER BY Customer_Full_Name ASC",
+                    "SELECT SR.Selling_Request_ID FROM Selling_Request AS SR LEFT JOIN Product AS PD ON SR.Selling_Request_ID=PD.Selling_Request_ID WHERE PD.Selling_Request_ID IS NULL AND SR.Selling_Request_Status=2 ORDER BY SR.Selling_Request_ID ASC",
                     false,
                     true,
                     null,
@@ -167,7 +163,7 @@ public class UICtrl_AddItem {
                 new ListViewRowDataWrapper<String>(tmpt_str, tmpt_str)
             );
         }
-        comboBox_custName.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
+        selectedSRID.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
     }
 }
 
