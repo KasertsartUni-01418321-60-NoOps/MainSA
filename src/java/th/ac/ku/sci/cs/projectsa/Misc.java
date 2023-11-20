f02fc52500159a1efc09d728f16ffd8c09a99495
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-20 22:51:37 +0700

    intermediate edit to 1 UI class + Misc.java edited

--- src/java/th/ac/ku/sci/cs/projectsa/Misc.java
+++ src/java/th/ac/ku/sci/cs/projectsa/Misc.java
@@ -4,7 +4,9 @@ public class Misc {
     public static final String[] ThaiStr_DataSpec_Status_SR= {"รอการนัดตรวจสอบสภาพฯ","ปฏิเสธการรับซื้อแล้ว","รับซื้อแล้ว"};
     public static final String[] ThaiStr_DataSpec_Status_pd= {"รอการประกาศขาย","พร้อมขาย","ขายแล้ว/กำลังส่ง","ส่งแล้ว"};
     public static final double choosenDefaultValueFor_PaidAmount_AtCheckItemPAge=25000;
+    public static final double choosenDefaultValueFor_PaidAmount_AtAddItemPAge=30000;
     public static double choosenStepValueFor_PaidAmount_AtCheckItemPAge=100;
+    public static double choosenStepValueFor_PaidAmount_AtAddItemPAge=100;
 
     public static String[] rickrollLyrics = new String[] {
         "We're no strangers to love",
--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_AddItem.java
@@ -2,21 +2,82 @@ package th.ac.ku.sci.cs.projectsa.uictrl;
 
 import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
+import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataSpec;
 import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataValidation;
 import th.ac.ku.sci.cs.projectsa.Misc.ListViewRowDataWrapper;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory;
+import javafx.scene.input.KeyEvent;
 
 public class UICtrl_AddItem {
     @FXML private ComboBox<ListViewRowDataWrapper<String>> selectedSRID;
     @FXML private TextArea rpDetail;
-    @FXML private Spinner<Double> sellPrice;
+    @FXML private Spinner<Double> spinnerDouble_Price;
 
-    @FXML private void initialize() throws java.sql.SQLException{
+    @FXML private void initialize() throws java.sql.SQLException {
         try{
+            spinnerDouble_Price.setEditable(true);
+            spinnerDouble_Price.setValueFactory(new DoubleSpinnerValueFactory(DatabaseMnm.DataSpec.RANGE_MIN__Product_Price, DatabaseMnm.DataSpec.RANGE_MAX__Product_Price, Misc.choosenDefaultValueFor_PaidAmount_AtAddItemPAge, Misc.choosenStepValueFor_PaidAmount_AtAddItemPAge));
+            spinnerDouble_Price.getEditor().focusedProperty().addListener((observable, oldValue, newValue) -> {
+                try {
+                    if (!newValue) { // If the spinner editor loses focus
+                        helper_refreshSpinnerDouble_1();
+                    }
+                } catch (Throwable e) {
+                    MyExceptionHandling.handleFatalException(e);
+                }
+            });
+            spinnerDouble_Price.getEditor().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+                try{
+                    if (event.getCode() == javafx.scene.input.KeyCode.ENTER) {
+                        helper_refreshSpinnerDouble_1();
+                        event.consume();
+                    } else if (event.getCode() == javafx.scene.input.KeyCode.UP){
+                        helper_refreshSpinnerDouble_1(); // incase mal-value
+                        spinnerDouble_Price.increment();
+                        event.consume();
+
+                    } else if (event.getCode() == javafx.scene.input.KeyCode.DOWN){
+                        helper_refreshSpinnerDouble_1(); // incase mal-value
+                        spinnerDouble_Price.decrement();
+                        event.consume();
+                    }
+                } catch (Throwable e) {
+                    MyExceptionHandling.handleFatalException(e);
+                }
+            });
             comboBox_srID_Helper1();
-            
+            selectedSRID.setOnAction(event -> {
+                try{
+                    String srID = selectedSRID.getValue().ref;
+                    DatabaseMnm.Table tmpc_SQLTable = null;
+                    try {
+                        tmpc_SQLTable = (DatabaseMnm.Table) (DatabaseMnm.runSQLcmd(
+                                null,
+                                "SELECT SR.Selling_Request_Repairment_Description FROM Selling_Request AS SR WHERE SR.Selling_Request_ID=?",
+                                false,
+                                true,
+                                null,
+                                new Object[] {srID}
+                        )[1]);
+                    } catch (java.sql.SQLException e) {
+                        MyExceptionHandling.handleFatalException_simplev1(e, true, "MainApp|DatabaseMnm", null, null,
+                                "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดตรวจสอบความถูกต้องของไฟล์โปรแกรมและข้อมูลและตรวจสอบว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
+                        throw e;
+                    }
+                    String tmpk_rpmDesc= (String)(tmpc_SQLTable.cols[0].vals.get(0));
+                    if (tmpk_rpmDesc==null){
+                        rpDetail.set
+                    } else {
+
+                    }
+
+                } catch (Throwable e) {
+                    MyExceptionHandling.handleFatalException(e);
+                }
+            });
         } catch (Throwable e) {
             MyExceptionHandling.handleFatalException(e);
             throw e;
@@ -31,7 +92,7 @@ public class UICtrl_AddItem {
             throw e;
         }
     }
-    // TODO:
+
     @FXML private void onpressed_Button_Save() throws java.io.IOException,java.sql.SQLException {
         // try {
         //     // Note that this always got from DB, so no validation
@@ -164,6 +225,23 @@ public class UICtrl_AddItem {
             );
         }
         selectedSRID.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
+    }
+
+    private void helper_refreshSpinnerDouble_1() {
+        Double tmpk_data=null;
+        try {
+            tmpk_data = Double.parseDouble(spinnerDouble_Price.getEditor().getText());
+        } catch (NumberFormatException e) {
+            spinnerDouble_Price.getEditor().setText(spinnerDouble_Price.getValue().toString());
+            return;
+        }
+        // Optionally, round the entered value
+        Integer[] lenSpec = DataSpec.MINMAX_LENGTH_OF_ATTRIBS.get("Selling_Request_Paid_Amount");
+        tmpk_data=DatabaseMnm.DataTransformation.doubleLengthCropping(tmpk_data, lenSpec[0], lenSpec[1],false);
+        // Set the rounded value back to the spinner
+        spinnerDouble_Price.getEditor().setText(tmpk_data.toString());
+        spinnerDouble_Price.getValueFactory().setValue(tmpk_data);
+        
     }
 }
 
