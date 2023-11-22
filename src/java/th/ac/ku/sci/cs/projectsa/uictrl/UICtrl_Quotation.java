9b15705361c52da5235d97008e2c082f7ae4e548
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-22 16:20:50 +0700

    complete QUOTATION paeg

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Quotation.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_Quotation.java
@@ -2,6 +2,7 @@ package th.ac.ku.sci.cs.projectsa.uictrl;
 
 import th.ac.ku.sci.cs.projectsa.uictrl.*;
 import th.ac.ku.sci.cs.projectsa.*;
+import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataSpec;
 import th.ac.ku.sci.cs.projectsa.DatabaseMnm.DataValidation;
 import th.ac.ku.sci.cs.projectsa.Misc.ListViewRowDataWrapper;
 
@@ -12,11 +13,12 @@ import com.github.saacsos.FXRouter;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.SpinnerValueFactory.DoubleSpinnerValueFactory;
+import javafx.scene.input.KeyEvent;
 
 public class UICtrl_Quotation {
     @FXML private ComboBox<ListViewRowDataWrapper<String>> comboBox_custName;
     @FXML private TextArea textArea_Loc;
-    // TODO:
     @FXML private Spinner<Double> spinner_Price;
     @FXML private Button button_defaultLoc;
     private String custLoc=null,pdID=null;
@@ -54,6 +56,37 @@ public class UICtrl_Quotation {
                     MyExceptionHandling.handleFatalException(e);
                 }
             });
+            // SEP
+            spinner_Price.setEditable(true);
+            spinner_Price.setValueFactory(new DoubleSpinnerValueFactory(DatabaseMnm.DataSpec.RANGE_MIN__Buy_Request_Transportation_Price, DatabaseMnm.DataSpec.RANGE_MAX__Buy_Request_Transportation_Price, Misc.choosenDefaultValueFor_TPrice_AtQuotationPAge, Misc.choosenStepValueFor_TPrice_AtQuotationPAge));
+            spinner_Price.getEditor().focusedProperty().addListener((observable, oldValue, newValue) -> {
+                try {
+                    if (!newValue) { // If the spinner editor loses focus
+                        helper_refreshSpinnerDouble_1();
+                    }
+                } catch (Throwable e) {
+                    MyExceptionHandling.handleFatalException(e);
+                }
+            });
+            spinner_Price.getEditor().addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+                try{
+                    if (event.getCode() == javafx.scene.input.KeyCode.ENTER) {
+                        helper_refreshSpinnerDouble_1();
+                        event.consume();
+                    } else if (event.getCode() == javafx.scene.input.KeyCode.UP){
+                        helper_refreshSpinnerDouble_1(); // incase mal-value
+                        spinner_Price.increment();
+                        event.consume();
+
+                    } else if (event.getCode() == javafx.scene.input.KeyCode.DOWN){
+                        helper_refreshSpinnerDouble_1(); // incase mal-value
+                        spinner_Price.decrement();
+                        event.consume();
+                    }
+                } catch (Throwable e) {
+                    MyExceptionHandling.handleFatalException(e);
+                }
+            });
         } catch (Throwable e) {
             MyExceptionHandling.handleFatalException(e);
             throw e;
@@ -68,9 +101,15 @@ public class UICtrl_Quotation {
             throw e;
         }
     }
-    @FXML private void onpressed_Button_Save() throws java.io.IOException,java.sql.SQLException {
+    
+     @FXML private void onpressed_DefaultLocButton() {
+        textArea_Loc.setText(custLoc);
+    }
+
+    @FXML private void onpressed_SaveButton() throws java.io.IOException,java.sql.SQLException {
         try {
             // Note that this always got from DB, so no validation
+            // REMARK: for pdID, it is no need of FK/PK validation because the flow of UI/data operation is designed
             String formval_custName=null;
             try {formval_custName = comboBox_custName.getSelectionModel().getSelectedItem().ref;}
             catch (NullPointerException e) {
@@ -79,76 +118,44 @@ public class UICtrl_Quotation {
             }
             Long formval_Date= LocalDate.now().toEpochDay();
             String formval_Loc = textArea_Loc.getText();
+            helper_refreshSpinnerDouble_1();
+            Double formval_tprice = spinner_Price.getValue();
             // [VALIDZONE]
             DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
-            // DISABLED: due to above reason
-            // DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON tmpReason;
-            // tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Customer_Full_Name(formval_custName);
-            // if (tmpReason ==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.VALUE_NOT_EXISTED_AT_REFERENCED_COL) {
-            //     helper2();
-            //     return;
-            // } else if (tmpReason != null) {
-            //     helper1();
-            //     return;
-            // }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Brand(formval_brand);
+            tmpReason = DataValidation.PerAttributeValidation.check__BUY_REQUEST__Buy_Request_Location(formval_Loc);
             if (tmpReason != null) {
                 helper1();
                 return;
             }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Model(formval_model);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Date(formval_meetDate);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            if (!DatabaseMnm.DataValidation.ForMoreBussinessConstraint.checkDateAsEpochTimeIsNotPast(formval_meetDate)) {
-                helper3();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Meet_Location(formval_custLoc);
-            if (tmpReason != null) {
-                helper1();
-                return;
-            }
-            tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_Product_Looks(formval_productLooks);
+            tmpReason = DataValidation.PerAttributeValidation.check__BUY_REQUEST__Buy_Request_Transportation_Price(formval_tprice);
             if (tmpReason != null) {
                 helper1();
                 return;
             }
             // [END VALID ZONE]
-            String tmpt_str = null;
-            while (true) {
-                tmpt_str= Misc.generateRandomID();
-                tmpReason = DataValidation.PerAttributeValidation.check__SELLING_REQUEST__Selling_Request_ID(tmpt_str);
-                if (tmpReason==DatabaseMnm.DataValidation.DATAVALID_DECLINED_REASON.REPEATED_VAL_OF_COL_PK) {
-                    continue;
-                } else if (tmpReason!=null) {
-                    throw new MyExceptionHandling.UserRuntimeException("Code should not be unreachable here.");
-                } else { break;}
-            }
             try{
                 DatabaseMnm.runSQLcmd(
                     null,
-                    "INSERT INTO Selling_Request VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?,?);",
+                    "INSERT INTO Buy_Request VALUES (?, ?, ?, ?, ?);",
                     true,
                     false,
                     null,
                     new Object[] {
-                        tmpt_str,
                         formval_custName,
-                        formval_brand,
-                        formval_model,
-                        formval_productLooks,
-                        formval_meetDate,
-                        formval_custLoc,
-                        Double.class,
-                        (long)0,
-                        String.class
+                        pdID,
+                        formval_Date,
+                        formval_tprice,
+                        formval_Loc
+                    }
+                );
+                DatabaseMnm.runSQLcmd(
+                    null,
+                    "UPDATE Product SET Product_Status=2 WHERE Product_ID=?;",
+                    true,
+                    false,
+                    null,
+                    new Object[] {
+                        pdID
                     }
                 );
                 DatabaseMnm.mainDbConn.commit();
@@ -157,7 +164,7 @@ public class UICtrl_Quotation {
                 "<html>โปรแกรมเกิดข้อผิดพลาดร้ายแรง โดยเป็นปัญหาของระบบฐานข้อมูลแบบ SQL ซึ่งทำงานไม่ถูกต้องตามที่คาดหวังไว้<br/>โดยสาเหตุอาจจะมาจากฝั่งของผู้ใช้หรือของบั๊กโปรแกรม โปรดตรวจสอบความถูกต้องของไฟล์โปรแกรมและข้อมูลและตรวจสอบว่าโปรแกรมสามารถเข้าถึงไฟล์ได้อย่างถูกต้อง<br/>โดยข้อมูลของปัญหาได้ถูกระบุไว้ด้านล่างนี้:</html>");
                 throw e;
             };
-            Main.switchToSpecificPagename("buy_history");
+            onBack_Button();
         } catch (Throwable e) {
             MyExceptionHandling.handleFatalException(e);
             throw e;
@@ -171,6 +178,23 @@ public class UICtrl_Quotation {
     private void helper2() {
         Main.showAlertBox(Main.getPrimaryStage(), AlertType.ERROR, "การเพิ่มข้อมูลผิดพลาด",
 						"ไม่สามารถเพ่ิมข้อมูลสัญญาขายได้", "เนื่องจากชื่อลูกค้ายังไม่ได้ถูกเลือก", false);
+    }
+
+    private void helper_refreshSpinnerDouble_1() {
+        Double tmpk_data = null;
+        try {
+            tmpk_data = Double.parseDouble(spinner_Price.getEditor().getText());
+        } catch (NumberFormatException e) {
+            spinner_Price.getEditor().setText(spinner_Price.getValue().toString());
+            return;
+        }
+        // Optionally, round the entered value
+        Integer[] lenSpec = DataSpec.MINMAX_LENGTH_OF_ATTRIBS.get("Buy_Request_Transportation_Price");
+        tmpk_data = DatabaseMnm.DataTransformation.doubleLengthCropping(tmpk_data, lenSpec[0], lenSpec[1], false);
+        // Set the rounded value back to the spinner
+        spinner_Price.getEditor().setText(tmpk_data.toString());
+        spinner_Price.getValueFactory().setValue(tmpk_data);
+
     }
 
     private void comboBox_custName_Helper1() throws java.sql.SQLException {
@@ -198,9 +222,6 @@ public class UICtrl_Quotation {
             );
         }
         comboBox_custName.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
-    }
-    @FXML private void onpressed_DefaultLocButton() {
-        textArea_Loc.setText(custLoc);
     }
 }
 
