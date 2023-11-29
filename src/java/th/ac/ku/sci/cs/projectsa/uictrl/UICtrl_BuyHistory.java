53954baa9dae973406e837cef9487e2750e78511
Nuttapong Punpipat <nuttapongpunpipat@gmail.com>
2023-11-29 15:15:26 +0700

    apply "ListView Colorize Row" to this ctrl

--- src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_BuyHistory.java
+++ src/java/th/ac/ku/sci/cs/projectsa/uictrl/UICtrl_BuyHistory.java
@@ -12,6 +12,9 @@ import javafx.event.EventHandler;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 
 public class UICtrl_BuyHistory {
     @FXML
@@ -31,6 +34,48 @@ public class UICtrl_BuyHistory {
                 button_CreatePurchase.setDisable(true);
             }
             button_sortByDate.setDisable(true);
+            srListView.setCellFactory(param -> new ListCell<ListViewRowDataWrapper<String>>() {
+                @Override
+                protected void updateItem(ListViewRowDataWrapper<String> item, boolean empty) {
+                    super.updateItem(item, empty);
+    
+                    if (empty || item == null) {
+                        setText(null);
+                        setBackground(null);
+                    } else {
+                        setText(item.toString());
+                        int tmpt_int = (int)item.params[0];
+                        int[] tmpt_arr_int = Misc.javafxListViewCellRowBgColorBasedOnPdType[tmpt_int];
+                        setBackground(new Background(new BackgroundFill(
+                            Color.rgb(tmpt_arr_int[0],tmpt_arr_int[1],tmpt_arr_int[2]),
+                            null, null)));
+                        tmpt_arr_int = Misc.getHighestContrastFGColor(tmpt_arr_int);
+                        setTextFill(Color.rgb(tmpt_arr_int[0],tmpt_arr_int[1],tmpt_arr_int[2]));
+                        // copy จากข้างบนมาเลยๆ
+                        selectedProperty().addListener((observable, oldValue, newValue) -> {
+                            int tmpt_int_1=0;
+                            int[] tmpt_arr_int_1=null;
+                            if (isSelected()) {
+                                tmpt_int_1 = (int)item.params[0];
+                                tmpt_arr_int_1 = Misc.javafxListViewCellRowBgColorBasedOnPdType[tmpt_int_1];
+                                setBackground(new Background(new BackgroundFill(
+                                    Color.rgb(255-tmpt_arr_int_1[0],255-tmpt_arr_int_1[1],255-tmpt_arr_int_1[2]),
+                                    null, null)));
+                                tmpt_arr_int_1 = Misc.getHighestContrastFGColor(tmpt_arr_int_1);
+                                setTextFill(Color.rgb(255-tmpt_arr_int_1[0],255-tmpt_arr_int_1[1],255-tmpt_arr_int_1[2]));
+                            } else {
+                                tmpt_int_1 = (int)item.params[0];
+                                tmpt_arr_int_1 = Misc.javafxListViewCellRowBgColorBasedOnPdType[tmpt_int_1];
+                                setBackground(new Background(new BackgroundFill(
+                                    Color.rgb(tmpt_arr_int_1[0],tmpt_arr_int_1[1],tmpt_arr_int_1[2]),
+                                    null, null)));
+                                tmpt_arr_int_1 = Misc.getHighestContrastFGColor(tmpt_arr_int_1);
+                                setTextFill(Color.rgb(tmpt_arr_int_1[0],tmpt_arr_int_1[1],tmpt_arr_int_1[2]));
+                            }
+                        });
+                    }
+                }
+            });
             helper_listViewUpdate(-1);
             srListView.setOnMouseClicked(new EventHandler<MouseEvent>() {
                 @Override
@@ -147,21 +192,29 @@ public class UICtrl_BuyHistory {
                 tmpk_Product_Status = DatabaseMnm.DataSpec.STATUS_Product.values()[tmpt_long.intValue()];
             }
             String tmpt_str_1 = null;
+            int tmpt_int=0;
             if (tmpk_Selling_Request_Status == STATUS_Selling_Request.WaitForCheck) {
+                tmpt_int=0;
                 tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_SR[0];
             } else if (tmpk_Selling_Request_Status == STATUS_Selling_Request.Acceapted) {
                 if (tmpk_Product_Status == null) {
+                    tmpt_int=2;
                     tmpt_str_1 = "รอนำสินค้าเข้าคลัง";
                 } else if (tmpk_Product_Status == STATUS_Product.NotYetSale) {
+                    tmpt_int=3;
                     tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_pd[0];
                 } else if (tmpk_Product_Status == STATUS_Product.ForSale) {
+                    tmpt_int=4;
                     tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_pd[1];
                 } else if (tmpk_Product_Status == STATUS_Product.SaledAndWaitForSend) {
+                    tmpt_int=5;
                     tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_pd[2];
                 } else {
+                    tmpt_int=6;
                     tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_pd[3];
                 }
             } else {
+                tmpt_int=1;
                 tmpt_str_1 = Misc.ThaiStr_DataSpec_Status_SR[1];
             }
             Long tmpu_epochTimeData = DatabaseMnm.convertIntegerAlikeSQLColToLong(
@@ -176,7 +229,7 @@ public class UICtrl_BuyHistory {
                     + tmpt_str_1 + tmpt_str_2 + "] โดยคุณ \"" + tmpk_Customer_Full_Name + "\" (ยี่ห้อ/รุ่น: \""
                     + tmpk_Selling_Request_Brand + "\"/\"" + tmpk_Selling_Request_Model + "\")";
             tmpc_SQLTable__listViewRowDataWrapper.add(
-                    new ListViewRowDataWrapper<String>(tmpk_Selling_Request_ID, tmpk_repr));
+                    new ListViewRowDataWrapper<String>(tmpk_Selling_Request_ID, tmpk_repr,new Object[] {tmpt_int}));
         }
         srListView.getItems().addAll(tmpc_SQLTable__listViewRowDataWrapper);
     }
